8356cb6dc79679088003d77f8555b0c8ca678579
Héctor Amparán <hctramprn@gmail.com>
2022-04-17 01:15:38 -0500

    Added the two-fer exercise

---
+++ two-fer/two_fer.py
@@ -0,0 +1,6 @@
+def two_fer(name='you'):
+    """This function returns a custom string.
+
+    This string returns a custom string in case a name is given or returns default you in case no parameter is given.
+    """
+    return f'One for {name}, one for me.'
