64216b130d7b04690c3ae87ea640073a0b9af65d
Héctor Amparán <hctramprn@gmail.com>
2022-05-26 18:38:37 -0500

    Added the resistor color exercise

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,10 @@
+resistors = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4,
+             'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
+
+
+def color_code(color):
+    return resistors[color]
+
+
+def colors():
+    return [color for color in resistors]
