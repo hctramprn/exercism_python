b59d03e8bc8cb970fe20ca271ebeb2ac6e350467
Héctor Amparán <hctramprn@gmail.com>
2022-05-26 18:28:36 -0500

    Added the reverse string exercise

--- tests.py
+++ tests.py
@@ -1,36 +1,6 @@
 
 def run():
-    students = {'Héctor': 2, 'Bodo': 1, 'Alejandra': 2}
-    added_students = []
-    grade = 2
-    name = 'Héctor'
-
-    if name not in students.keys():
-        students[name] = grade
-        added_students.append(True)
-    else:
-        added_students.append(False)
-    
-    x = [name[1] for name in sorted([(students[student], student) for student in students])]
-    print(x)
-
-
-
-
-    #print([student[1] for student in sorted(students)])
-    #print([student_name for ])
-
-    # name = 'Hector'
-    # grade = 2
-
-    # if name not in students:
-    #     students.append((name, grade))
-    #     added_students.append(True)
-    # else:
-    #     added_students.append(False)
-    
-    # print(students)
-    # print(added_students)
+    pass
 
 
 if __name__ == '__main__':
