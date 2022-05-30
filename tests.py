6f9fafa792b77c3821c34b607e5e88e12bb70db1
Héctor Amparán <hctramprn@gmail.com>
2022-05-30 12:43:53 -0500

    Added the difference of squares exercise

--- tests.py
+++ tests.py
@@ -1,18 +1,7 @@
 
 
 def run():
-    candidates = ["Eons", "ONES"]
-    word = "nose"
-    candidates = [wrd for wrd in candidates if wrd.lower() != word.lower()]
-    word = sorted(word.lower())
-    words = []
-    for candidate in candidates:
-        if word == sorted(candidate.lower()):
-            words.append(candidate)
-    print(words)
-
-
-
+    print(sum(range(10)**2))
 
 
 if __name__ == '__main__':
