dbe02c562e710f25442be28f73896059a8f8844a
Héctor Amparán <hctramprn@gmail.com>
2022-05-31 11:23:05 -0500

    Added the perfect numbers exercise

--- tests.py
+++ tests.py
@@ -1,5 +1,10 @@
 def run():
-    pass
+    number = 16
+    factors = []
+    for num in range(1,number):
+        if number % num == 0:
+            factors.append(num)
+    print(factors)
 
 if __name__ == '__main__':
     run()
