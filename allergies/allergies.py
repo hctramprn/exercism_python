847f3c37065289ec4ab13726a4c4e6fe9b54497b
Héctor Amparán <hctramprn@gmail.com>
2022-08-08 12:14:53 -0500

    Added a draft of the allergies exercise

---
+++ allergies/allergies.py
@@ -0,0 +1,46 @@
+from itertools import permutations
+
+ALLERGIES = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
+             'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
+
+
+class Allergies:
+
+    def __init__(self, score):
+        self.score = self.score_adjust(score)
+        self.combination = self.combinations()
+
+    def score_adjust(self, score):
+        score = score
+        if score > 256:
+            pwr = 0
+            while score - (2**pwr) > 255:
+                pwr += 1
+            return score - (2**pwr)
+        return score
+
+    def combinations(self):
+        perm = list(permutations([1, 2, 4, 8, 16, 32, 64, 128], 8))
+        for combination in perm:
+            n = 0
+            for i in range(8):
+                n += combination[i]
+                if self.score - n == 0:
+                    return combination[:i + 1]
+
+    def allergic_to(self, item):
+        if self.score == 0:
+            return False
+        return ALLERGIES[item] in self.combination
+
+    @property
+    def lst(self):
+        lst = self.combination
+        if lst != None:
+            allergies = []
+            for comb in lst:
+                for allergen, score in ALLERGIES.items():
+                    if comb == score:
+                        allergies.append(allergen)
+            return allergies
+        return []
--- tests.py
+++ tests.py
@@ -1,6 +1,26 @@
+from itertools import permutations
+
+ALLERGIES = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
+             'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
+
 
 def run():
-    pass
+    score = 257
+    if score > 256:
+        pwr = 0
+        while score - (2**pwr) > 255:
+            pwr += 1
+        return score - (2**pwr)
+    return score
+
+    # lst = [16, 1, 8]
+    # allergies = []
+    # for comb in sorted(lst):
+    #     for allergen, score in ALLERGIES.items():
+    #         if comb == score:
+    #             allergies.append(allergen)
+    # print(allergies)
+
 
 if __name__ == '__main__':
     run()
