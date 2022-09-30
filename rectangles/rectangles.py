77414acba8214825a6818e6ae0f31ab9cf335c29
Héctor Amparán <hctramprn@gmail.com>
2022-09-29 21:18:45 -0500

    Added the final solution for the rectangle counter exercise

--- rectangles/rectangles.py
+++ rectangles/rectangles.py
@@ -1,28 +1,48 @@
 def rectangles(strings):
-    # find vertices
-    vertices = []
-    for line in strings:
-        line_ver_temp = []
-        for i in range(len(line)):
-            if line[i] == '+':
-                line_ver_temp.append(i)
-        vertices.append(line_ver_temp)
-    # print(vertices)
+    # find all the vertices
+    vertices_coordinates = []
+    for i in range(len(strings)):
+        for j in range(len(strings[i])):
+            if strings[i][j] == '+':
+                vertices_coordinates.append((j, i))
+    # print(vertices_coordinates)
 
-    # find possible square sides
-    sides_possible = []
-    for ver_sublist in vertices:
-        index = 0
-        for ver in ver_sublist:
-            for j in range(len(ver_sublist)):
-                if ver_sublist[j] - ver_sublist[index] > 0:
-                    sides_possible.append((ver_sublist[index], ver_sublist[j]))
-            index += 1
+    # validate each possible rectangle
+    possible_rectangles = []
+    # starting coordinates 1
+    for first_coordinate in vertices_coordinates:
+        x1, y1 = first_coordinate[0], first_coordinate[1]
+        # test each of the other coordinates in the list
+        for second_coordinate in vertices_coordinates:
+            if first_coordinate != second_coordinate:
+                x2, y2 = second_coordinate[0], second_coordinate[1]
+                # checks if the 4 possible vertices exist in the list and appends them
+                vertices_temp = set(((x1, y1), (x1, y2), (x2, y2), (x2, y1)))
+                if len(vertices_temp) == 4 and vertices_temp.issubset(set(vertices_coordinates)) and vertices_temp not in possible_rectangles:
+                    possible_rectangles.append(vertices_temp)
 
-    # find the combinations of sides
-    combinations_per_side = [sides_possible.count(v) - 1 for v in set(sides_possible)]
+    # test each of the possible rectangles
     rectangles = 0
-    for num in combinations_per_side:
-        for n in range(num + 1):
-            rectangles += n
+    for rectangle_set in possible_rectangles:
+        lenght_x = set([x[0] for x in rectangle_set])
+        lenght_y = set([x[1] for x in rectangle_set])
+        
+        # test horizontals
+        tested_sides = 0
+        for row in lenght_y:
+            if set(strings[row][min(lenght_x):max(lenght_x) + 1]).issubset('+-'):
+                tested_sides += 1
+        
+        # test verticals
+        for column in lenght_x:
+            col_temp = ''
+            for row in range(min(lenght_y), max(lenght_y) + 1):
+                col_temp += strings[row][column]
+            if set(col_temp).issubset('+|'):
+                tested_sides += 1
+
+        # check sides
+        if tested_sides == 4:
+            rectangles += 1
+    
     return rectangles
